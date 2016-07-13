3aa21953113d8a860d55d3051fd4f13e9a0c088d
LinCX <957109587@qq.com>
2016-07-13 19:06:52 +0800

    页面问题修复完成，新增ProgressDialog优化交互

--- app/src/main/java/socialnews/linccy/com/socialnews/fragment/NewsListFragment.java
+++ app/src/main/java/socialnews/linccy/com/socialnews/fragment/NewsListFragment.java
@@ -1,8 +1,12 @@
 package socialnews.linccy.com.socialnews.fragment;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,11 +25,12 @@ import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
+import socialnews.linccy.com.socialnews.R;
+import socialnews.linccy.com.socialnews.adapter.NewsItemAdapter;
 import socialnews.linccy.com.socialnews.utils.Constant;
 import socialnews.linccy.com.socialnews.utils.NewsItem;
-import socialnews.linccy.com.socialnews.adapter.NewsItemAdapter;
 import socialnews.linccy.com.socialnews.utils.NewsListResponseJson;
-import socialnews.linccy.com.socialnews.R;
+import socialnews.linccy.com.socialnews.view.SwipeLoadRefreshLayout;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -35,14 +40,18 @@ public class NewsListFragment extends BaseFragment {
      * The fragment argument representing the section number for this
      * fragment.
      */
-    private static final String ARG_SECTION_NUMBER = "section_number";
-    private static List<NewsItem> newsItems = new ArrayList<>();
+    Constant constant;
+    private List<NewsItem> newsItems = new ArrayList<>();
     RequestQueue mQueue;
-
+    private int sectionNumber;
+    NewsItemAdapter adapter;
+    @BindView(R.id.sl)
+    SwipeLoadRefreshLayout sl;
     @BindView(R.id.section_label)
     TextView textView;
     @BindView(R.id.recyclerView_NewsList)
     RecyclerView mRecyclerView;
+    ProgressDialog progressDialog;
 
     public NewsListFragment() {
     }
@@ -51,23 +60,32 @@ public class NewsListFragment extends BaseFragment {
      * Returns a new instance of this fragment for the given section
      * number.
      */
-    public static NewsListFragment newInstance(int sectionNumber) {
-        NewsListFragment fragment = new NewsListFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+//    public static NewsListFragment newInstance(int sectionNumber1) {
+//        NewsListFragment fragment = new NewsListFragment();
+//        fragment.sectionNumber = sectionNumber1;
+//        Bundle args = new Bundle();
+//        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
     @Override
     protected void initData() {
         super.initData();
+        sectionNumber = getArguments().getInt("STATE");
+//        textView.setText("fuck");
+        Bundle bundle = new Bundle();
+        constant = new Constant(sectionNumber, 10);
         mQueue = Volley.newRequestQueue(getContext());
-        textView.setText("fuck");
-        getNewsData(Constant.SocialNewsURL);
         mRecyclerView.setHasFixedSize(false);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mRecyclerView.setAdapter(new NewsItemAdapter(newsItems));
+
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("数据加载中....");
+        progressDialog.show();
+        getNewsData(constant.getRefreshUrl());
+//        adapter = new NewsItemAdapter(newsItems);
+//        mRecyclerView.setAdapter(adapter);
 
     }
 
@@ -82,15 +100,17 @@ public class NewsListFragment extends BaseFragment {
             @Override
             public void onResponse(String response) {
                 NewsListResponseJson newsListJsonObject = gson.fromJson(response, NewsListResponseJson.class);
-                newsItems = newsListJsonObject.getNewslist();
+                newsItems = newsListJsonObject.getData();
+//                constant.setSid(newsItems.get(newsItems.size()).getId());
+                progressDialog.cancel();
+                adapter = new NewsItemAdapter(newsItems);
+                mRecyclerView.setAdapter(adapter);
             }
         }, new Response.ErrorListener() {
 
             @Override
             public void onErrorResponse(VolleyError error) {
-                if (newsItems == null) {
-                    System.out.print("aaaaaaaaa");
-                }
+                Log.e("网络错误", "104");
             }
         }) {
             @Override
@@ -100,20 +120,34 @@ public class NewsListFragment extends BaseFragment {
                 return headers;
             }
         };
-        if (newsItems == null) {
-            System.out.print("aaaaaaaaa");
-        }
         mQueue.add(stringRequest);
-        mQueue.start();
     }
 
     @Override
     protected int getLayoutId() {
-        return R.layout.fragment_home;
+        return R.layout.fragment_home_child;
     }
 
     @Override
     public void onClick(View v) {
 
+    }
+
+    @Override
+    protected void setListener() {
+        super.setListener();
+        sl.setOnLoadListener(new SwipeLoadRefreshLayout.OnLoadListener() {
+
+            @Override
+            public void onLoad() {
+
+            }
+        });
+        sl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getNewsData(constant.getRefreshUrl());
+            }
+        });
     }
 }
